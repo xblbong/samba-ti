190f52d4e0cb05c3f1af17625af01a8e3b9c62fd
xblbong <sblhh.m@gmail.com>
2025-08-24 18:26:19 +0700

    style: chage logo profile

--- resources/views/course/course.blade.php
+++ resources/views/course/course.blade.php
@@ -2,20 +2,20 @@
 @section('content_course')
                     {{-- Jumbotron --}}
                     <div class="w-full mb-6 wrap7">
-                        <div class="p-6 max-[750px]:p-0 bg-gradient-to-r from-cyan-300 to-violet-900 shadow z-30 rounded-lg full7 hilang-margin7">
+                        <div class="p-6 max-[750px]:p-0 bg-gradient-to-r from-blue-400 to-cyan-400 shadow z-30 rounded-lg full7 hilang-margin7">
                             <div class="wrapper-jumb">
                                 <div class="flex px-4 py-3">
                                     <div class="py-4 max-[550px]:py-1 w-[70%]">
                                         <div class="relative h-full">
                                             <div class="">
-                                                <h1 class="text-white font-bold text-3xl max-[550px]:text-lg max-[400px]:text-sm max-[650px]:text-2xl tracking-wide min-[1150px]:text-5xl">Ayo Selesaikan Tugasmu!</h1>
+                                                <h1 class="text-white font-bold text-3xl max-[550px]:text-lg max-[400px]:text-sm max-[650px]:text-2xl tracking-wide min-[1150px]:text-5xl">Selamat datang, <span class="text-yellow-300">Namakamu!</span></h1>
                                                 <p class="text-white text-sm max-[400px]:text-[10px] max-[550px]:text-xs max-[650px]:text-sm max-[650px]:w-[85%] w-[50%] tracking-wide min-[1150px]:text-xl min-[1150px]:mt-2">Deadline tugasmu semakin dekat segera selesaika tugasmu.</p>
                                                 <a href="{{ route('tugasku') }}" class="absolute bottom-0 text-white font-bold max-[400px]:text-sm max-[650px]:text-base text-xl min-[1150px]:text-2xl"><span class="flex justify-center items-center">Lihat Tugas<i data-feather="arrow-right-circle" class="text-white ml-2"></i></span></a>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="w-[30%]">
-                                        <img src="{{ asset('assets/img/jumb.png') }}" alt="" class="min-[1150px]:w-[80%]">
+                                        <img src="{{ asset('assets/img/samba/kapal.svg') }}" alt="logo-kapal" class="min-[1150px]:w-[80%]">
                                     </div>
                                 </div>
                             </div>
